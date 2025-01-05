8d619796e5a63a286c351d8b15158cc4a8eb8630
aziznafas <aziz.ba@nafafs.com>
2025-01-05 05:55:08 +0000

    clean code v2

--- src/shared/Footer/Footer.tsx
+++ src/shared/Footer/Footer.tsx
@@ -1,10 +1,8 @@
-import Link from 'next/link';
 import React from 'react';
 
 import { footerData } from '@/data/content';
 
 import Logo from '../Logo/Logo';
-import FooterBanner from './FooterBanner';
 import Subscribe from './Subscribe';
 
 const Footer: React.FC = () => {
