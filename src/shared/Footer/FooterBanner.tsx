dc0e74c19182aec8ce4286ba9234fc4585e35994
aziznafas <aziz.ba@nafafs.com>
2025-01-13 17:10:56 +0000

    gradient

--- src/shared/Footer/FooterBanner.tsx
+++ src/shared/Footer/FooterBanner.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-
 import { footerBannerData } from "@/data/content";
 import Heading from "@/shared/Heading/Heading";
 
-
-
 const FooterBanner = () => {
   return (
-    <div className="rounded-2xl bg-[#f37975] py-8 px-3 md:py-2 text-white sm:py-2">
+    <div className="rounded-2xl bg-gradient-to-r from-[#f85647] via-[#fc9cac] to-[#f85647] py-8 px-3 md:py-2 text-white sm:py-2">
       <Heading
         className="mb-0 text-center text-2xl sm:text-2xl"
         isMain
